380a0a3a720a12326e2e369028f63afc28008eb3
Daniel O'Connell <github@ahiru.pl>
2023-04-24 22:58:02 +0200

    coda tests

--- index.js
+++
@@ -1,9 +0,0 @@
-import parse from "./parser/main.js";
-import { logError } from "./parser/utils.js";
-
-try {
-  await parse();
-} catch (e) {
-  await logError(e.message, {});
-  process.exit(1);
-}
---
+++ jest.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    testEnvironment: 'node',
+    collectCoverage: true,
+    coverageReporters: ['text-summary', 'lcov'],
+    coverageDirectory: 'coverage',
+    verbose: true
+};
