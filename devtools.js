c5f14a4bd389f38eba6106ef5c0e1f684f4f4159
Chris Rimmer <chris@rimmer.wtf>
2023-05-23 14:45:20 +0000

    Formatting

--- devtools.js
+++ devtools.js
@@ -1,47 +1,52 @@
-import yargs from "yargs"
-import { hideBin } from "yargs/helpers"
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
 import { getDocsClient } from "./parser/gdrive.js";
-import { parseDoc } from "./parser/parser.js"
-
+import { parseDoc } from "./parser/parser.js";
 
 yargs(hideBin(process.argv))
-    .usage('Usage: $0 <command> [options]')
-    .command(
-        'json <googleDocID>',
-        'Get the JSON for an answer doc',
-        async ({argv: {_: [_, documentId]}}) => {
-            try {
-                const docs = await getDocsClient()
-                const doc = (await docs.documents.get({ documentId })).data
-    
-                console.log(JSON.stringify(doc, null, 2))
-            } catch (e) {
-                if (e.code === 404) {
-                    console.error("Invalid doc ID")
-                }
-            }
-        }
-    )
-    .command(
-        'md <googleDocID>',
-        'Get the rendered Markdown for an answer doc',
-        async ({argv: {_: [_, documentId]}}) => {
-            try {
-                const docs = await getDocsClient()
-                const doc = (await docs.documents.get({ documentId })).data
+  .usage("Usage: $0 <command> [options]")
+  .command(
+    "json <googleDocID>",
+    "Get the JSON for an answer doc",
+    async ({
+      argv: {
+        _: [_, documentId],
+      },
+    }) => {
+      try {
+        const docs = await getDocsClient();
+        const doc = (await docs.documents.get({ documentId })).data;
 
-                console.log((await parseDoc(doc)).md)
-            } catch (e) {
-                if (e.code === 404) {
-                    console.error("Invalid doc ID")
-                } else {
-                    console.error(e)
-                }
-            }
+        console.log(JSON.stringify(doc, null, 2));
+      } catch (e) {
+        if (e.code === 404) {
+          console.error("Invalid doc ID");
         }
-    )
-    .help('h')
-    .alias('h', 'help')
-    .argv
+      }
+    }
+  )
+  .command(
+    "md <googleDocID>",
+    "Get the rendered Markdown for an answer doc",
+    async ({
+      argv: {
+        _: [_, documentId],
+      },
+    }) => {
+      try {
+        const docs = await getDocsClient();
+        const doc = (await docs.documents.get({ documentId })).data;
 
+        console.log((await parseDoc(doc)).md);
+      } catch (e) {
+        if (e.code === 404) {
+          console.error("Invalid doc ID");
+        } else {
+          console.error(e);
+        }
+      }
+    }
+  )
+  .help("h")
+  .alias("h", "help").argv;
